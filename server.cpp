c4899303818bc1f16cd036c455b1aeebbbd2b4c8
harshadbiradar <harshad.ee20@rvce.edu.in>
2025-12-18 12:49:45 +0000

    with updated epoll folw along with basic client_handling flow

--- server.cpp
+++ server.cpp
@@ -13,6 +13,7 @@
 #define N_CLIENTS 5
 #define MAX_EVENTS 10
 #define N_WORKERS 5
+#define BUFFLEN 1024
 
 int epoll_fd=-1;
 int server_socket=-1;
@@ -24,7 +25,7 @@ struct HttpRequest{
     std::string method;
     std::string path;
 
-    std::unordered_map<std::string,std::string>headers;
+    // std::unordered_map<std::string,std::string>headers;
 
     std::string body;
 };
@@ -32,7 +33,7 @@ struct HttpResponse{
 
     int status_code;
     std::string reason;
-    std::unordered_map<std::string,std::string>headers;
+    // std::unordered_map<std::string,std::string>headers;
     std::string body;
 };
 
@@ -40,6 +41,9 @@ struct Connection{
     int fd;
     std::string read_buffer;
     std::string write_buffer;
+    // adding this to ensure thread_safety for this particular resource.
+    std::mutex m;
+    struct epoll_event event;
 
     enum class STATE{
         READING,
@@ -50,7 +54,7 @@ struct Connection{
     
     HttpRequest req;
     HttpResponse resp;
-    Connection(int fd):fd(fd){
+    Connection(int Cl_fd,struct epoll_event Cl_event):fd(Cl_fd),event(Cl_event){
         read_buffer.reserve(1024);
         write_buffer.reserve(1024);
         state=Connection::STATE::READING;
@@ -70,10 +74,37 @@ void terminate(){
     std::terminate();
 }
 
-void http_request_parser(){
+void http_request_parser(struct Connection &my_conn){
 
 }
 void http_response_parser(){
+
+}
+
+void read_func(struct Connection &my_conn){
+    auto lam=[&my_conn](){
+        std::unique_lock<std::mutex> lock(my_conn.m);
+        while(my_conn.state==Connection::STATE::PROCESSING||my_conn.state!=Connection::STATE::READING){
+            //here need to add Conditional_variable to handle it properly.
+        }
+        my_conn.state=Connection::STATE::PROCESSING;
+        char buffer[BUFFLEN];
+
+        int bytes_read;
+        while(bytes_read=recv(my_conn.fd,buffer,BUFFLEN-1,0)>0){
+            buffer[BUFFLEN]='\0';
+            my_conn.read_buffer.append(buffer);
+        }
+        if(bytes_read!=0){
+            std::cerr<<"Error in reading data for :"<<my_conn.fd<<std::endl;
+        }
+        else{
+            http_request_parser(my_conn);
+        }
+    };
+    Worker_thread.submit(lam);
+}
+void write_func(struct Connection &my_conn){
 
 }
 
@@ -96,19 +127,33 @@ void accept_all(struct epoll_event &event,std::unordered_map<int,Connection>&Liv
                 // also handle epoll_close,server_fd close
             }
         }
-        event.events = EPOLLIN | EPOLLOUT;
+        event.events = EPOLLIN;
         event.data.fd = client_fd;
         int flags = fcntl(client_fd, F_GETFL, 0);
         fcntl(client_fd, F_SETFL, flags | O_NONBLOCK);
         epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &event);
-        Connection conn(client_fd);
+        Connection conn(client_fd,event);
         Live_connections.emplace(client_fd,conn);
     }
 }
 
-void handle_client(){
+void handle_client(int fd,std::unordered_map<int,Connection>&Live_connections){
 
+    auto ref=Live_connections.find(fd);
+    if(ref==Live_connections.end()){
+        //error here..
+    }
+    auto &my_conn=ref->second;
+    if(my_conn.event.events==EPOLLIN){
+        read_func(my_conn);
+        //handle this when state is processing.
+    }
+    else if(my_conn.event.events==EPOLLOUT){
+        write_func(my_conn);
+    }
 }
+
+
 
 int main(){
     //creates a Thread_pool of 5 worker threads.
@@ -172,6 +217,7 @@ int main(){
             }
             else{
                 //client event here.
+                handle_client(events[i].data.fd,Live_conncetions);
             }
         }
     }
@@ -183,3 +229,25 @@ int main(){
     terminate();
     return 0;
 }
+
+
+//issues faced
+
+/*
+
+//only includes issues while building this particular program
+    (Blocking queue & Thread_pool were built as standalone program, previously).
+    
+--The first was designing the epoll_event handling,
+     differentiating from server_fd_handlilng, and clients_fd_handling.
+--Then post clinet_fd readiness, what to do read/write??(leveraged what was event.events_flag was set).
+--Post deciding read/wirte, how to get the work done by thread_pool without compromising, data robustness.
+    -- solved it using a mutex with each particular client_fd in un_map.
+--About handling and isolating the map_oprations plus, also controlling few map_ops so to check its
+    safety before performing ops.
+    --leveraged how un_maps are built.
+    --each pair in un_map has {key,value},
+    --now rehash only modifies the ref of this whole KV obj, the internal VALUE obj's ref remains same
+        till it is alive.
+    --Giving us leverage over ref for VALUE(rehash safe ref), can work independently on these refs.
+*/
